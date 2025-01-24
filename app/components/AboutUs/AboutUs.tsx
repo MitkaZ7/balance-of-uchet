48f3c0205412eed962e4e70ce8b3124f42cd1d0d
mitkazv <i@mitkadev.ru>
2025-01-24 20:11:33 +0300

    dev: markup done

--- app/components/AboutUs/AboutUs.tsx
+++ app/components/AboutUs/AboutUs.tsx
@@ -1,9 +1,24 @@
+"use client"
+import { useState, useEffect } from 'react';
 import styles from './AboutUs.module.scss'
 import Image from 'next/image'
 import photo from '../../../public/images/victory-photo.png'
-
+import photo_sm from '../../../public/images/victory-photo-sm.png'
 
 export const AboutUs = () => {
+
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 430);
+    };
+
+    handleResize(); // Проверяем при загрузке
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <section className={styles['about-section']}>
         <header className={styles['about-section__header']}>
@@ -27,15 +42,18 @@ export const AboutUs = () => {
                 <p>миллиона рублей было сэкономлено за полгода при индивидуальном согласовании снижения комиссии на банковские услуги для одной из наших компаний.</p>
               </li>
             </ul>
+            <div className={styles['about-section__photo-wrapper']}>
             <Image
               className={styles['about-section__photo']}
-              src={photo}
+              // src={photo}
+              src={isSmallScreen ? photo_sm : photo}
               alt="Picture of the author"
-      // width={500} automatically provided
-      // height={500} automatically provided
+              sizes="(max-width: 430px) 100px, (max-width: 768px) 500px, 1000px"
       // blurDataURL="data:..." automatically provided
       // placeholder="blur" // Optional blur-up while loading
     />
+            </div>
+            
 
         </div>
         <p className={styles['about-section__ending-phrase']}>Каждый клиент уникален, поэтому мы подходим к решению задач индивидуально.</p>
